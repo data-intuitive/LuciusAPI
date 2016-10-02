b176419af56ad281e3dc52f0ccdda4268b705497
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-10-02 21:58:18 +0200

    Small update

--- src/main/scala/com/dataintuitive/luciusapi/preprocess.scala
+++ src/main/scala/com/dataintuitive/luciusapi/preprocess.scala
@@ -4,7 +4,7 @@ import com.dataintuitive.luciuscore.io._
 
 import com.typesafe.config.Config
 import org.apache.spark._
-import spark.jobserver.{NamedRddSupport, SparkJob, SparkJobValid, SparkJobInvalid, SparkJobValidation}
+import spark.jobserver.{SparkJob, SparkJobValid, SparkJobInvalid, SparkJobValidation}
 import scala.util.Try
 
 /**
@@ -18,19 +18,34 @@ object preprocess extends SparkJob {
   /**
     * Make sure:
     *   - `locationFrom` and `locationTo` are provided
+    *   - Todo: Check if Amazon keys are exported (when using S3 locations)
     *   - Todo: Both locations exist
     */
   override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
-    //
+
     val locationFromOption = Try(config.getString("locationFrom")).toOption
     val locationToOption = Try(config.getString("locationTo")).toOption
 
-    (locationFromOption.isDefined, locationToOption.isDefined) match {
-      case (true,  true)   => SparkJobValid
-      case (false, true)   => SparkJobInvalid(s"Missing locationFrom config in POST")
-      case (true,  false)  => SparkJobInvalid(s"Missing locationTo config in POST")
-      case (false, false) => SparkJobInvalid(s"Missing both locationFrom and locationTo config in POST")
-    }
+    val fsS3AwsAccessKeyId      = sys.env.get("AWS_ACCESS_KEY_ID")
+    val fsS3AwsSecretAccessKey  = sys.env.get("AWS_SECRET_ACCESS_KEY")
+
+    val tests = Seq(
+      (locationFromOption.isDefined, "locationFrom not defined in POST config"),
+      (locationToOption.isDefined, "locationTo not defined in POST config"),
+      (fsS3AwsAccessKeyId.isDefined, "AWS Access key not exported in shell"),
+      (fsS3AwsSecretAccessKey.isDefined, "AWS Secret key not exported in shell")
+    )
+
+    val allTests = tests.reduce((a,b) => (a,b) match{
+      case ((true, l), (true, r))  => (true, "")
+      case ((true, l), (false, r)) => (false, r)
+      case ((false, l), (true, r)) => (false, l)
+      case ((false, l), (false, r)) => (false, l + ", " + r)
+    })
+
+    if (allTests._1) SparkJobValid
+    else SparkJobInvalid(allTests._2)
+
   }
 
   override def runJob(sc: SparkContext, config: Config): Any = {
@@ -56,16 +71,28 @@ object preprocess extends SparkJob {
     val geneAnnotationsFile = base + "featureData.txt"
     val compoundAnnotationsFile = base + "compoundAnnotations.tsv"
 
-    // Loading the data (V1)
-    val dbRelations = SampleCompoundRelationsIO.loadSampleCompoundRelationsFromFileV1(sc, sampleCompoundRelationsFile)
-    val dbAfterCA = dbRelations
-//    val compoundAnnotations = CompoundAnnotationsIO.loadCompoundAnnotationsFromFileV2(sc, compoundAnnotationsFile)
-//    val dbAfterCA = CompoundAnnotationsIO.updateCompoundAnnotationsV2(compoundAnnotations, dbRelations)
-    val tStats = StatsIO.loadStatsFromFile(sc, tStatsFile, toTranspose=true)
-    val pStats = StatsIO.loadStatsFromFile(sc, pStatsFile, toTranspose=true)
-    val dbAfterStatsTmp = StatsIO.updateStats(tStats, dbAfterCA, StatsIO.dbUpdateT)
-    val dbAfterStats = StatsIO.updateStats(pStats, dbAfterStatsTmp, StatsIO.dbUpdateP)
-    val db = RanksIO.updateRanks(dbAfterStats)
+    val db = version match {
+      case "v1" => {
+        // Loading the data (v1 format) -- All compound info is in the relations file
+        val dbRelations = SampleCompoundRelationsIO.loadSampleCompoundRelationsFromFileV1(sc, sampleCompoundRelationsFile)
+        val tStats = StatsIO.loadStatsFromFile(sc, tStatsFile, toTranspose = true)
+        val pStats = StatsIO.loadStatsFromFile(sc, pStatsFile, toTranspose = true)
+        val dbAfterStatsTmp = StatsIO.updateStats(tStats, dbRelations, StatsIO.dbUpdateT)
+        val dbAfterStats = StatsIO.updateStats(pStats, dbAfterStatsTmp, StatsIO.dbUpdateP)
+        RanksIO.updateRanks(dbAfterStats)
+      }
+      case "v2" => {
+        // Loading the data (v2 format) -- Compound info is in annotations file
+        val dbRelations = SampleCompoundRelationsIO.loadSampleCompoundRelationsFromFileV1(sc, sampleCompoundRelationsFile)
+        val compoundAnnotations = CompoundAnnotationsIO.loadCompoundAnnotationsFromFileV2(sc, compoundAnnotationsFile)
+        val dbAfterCA = CompoundAnnotationsIO.updateCompoundAnnotationsV2(compoundAnnotations, dbRelations)
+        val tStats = StatsIO.loadStatsFromFile(sc, tStatsFile, toTranspose = true)
+        val pStats = StatsIO.loadStatsFromFile(sc, pStatsFile, toTranspose = true)
+        val dbAfterStatsTmp = StatsIO.updateStats(tStats, dbAfterCA, StatsIO.dbUpdateT)
+        val dbAfterStats = StatsIO.updateStats(pStats, dbAfterStatsTmp, StatsIO.dbUpdateP)
+        RanksIO.updateRanks(dbAfterStats)
+      }
+    }
 
     // Loading gene annotations
     val genes = GenesIO.loadGenesFromFile(sc, geneAnnotationsFile)
