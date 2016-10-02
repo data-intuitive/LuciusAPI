605a2de849940b35a08c39edbe69ba64ca3f42b1
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-10-02 21:42:40 +0200

    BaseSparkContext for testing

---
+++ src/test/scala/com/dataintuitive/test/BaseSparkContextSpec.scala
@@ -0,0 +1,18 @@
+package com.dataintuitive.test
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object BaseSparkContextSpec {
+
+  lazy val conf = new SparkConf()
+    .setAppName("Test")
+    .setMaster("local[*]")
+  lazy val sc = new SparkContext(conf)
+
+}
+
+trait BaseSparkContextSpec {
+
+  lazy val sc = BaseSparkContextSpec.sc
+
+}
