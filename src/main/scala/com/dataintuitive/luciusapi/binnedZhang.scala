aefc14cb01d63877eaee3aa097cb207c6f0d18a1
Toni Verbeiren <toni.verbeiren@gmail.com>
2016-12-05 14:43:14 +0100

    First rough version of binnedZhang endpoint

---
+++ src/main/scala/com/dataintuitive/luciusapi/binnedZhang.scala
@@ -0,0 +1,80 @@
+package com.dataintuitive.luciusapi
+
+import com.dataintuitive.luciusapi.functions.BinnedZhangFunctions._
+import com.typesafe.config.Config
+import org.apache.spark._
+import spark.jobserver._
+
+import scala.util.Try
+
+/**
+  * Returns the Zhang/similarity scores for the whole database for a given query.
+  *
+  * Input:
+  *
+  * - __`query`__: The gene signature (any notation will do) as a space separated list
+  *
+  * - __`sorted`__: Should the result be sorted on by Zhang score (descending)
+  *
+  * - __`version`__: Version of the API. "v2" returns `info` and `header`.
+  *
+  * Remark: Make sure you check the signature first, through checkSignature
+  */
+object binnedZhang extends SparkJob with NamedRddSupport with Globals {
+
+  import Common._
+
+  val simpleChecks:SingleParValidations = Seq(
+    ("query",   (isDefined ,    "query not defined in POST config")),
+    ("query",   (isNotEmpty ,   "query is empty in POST config"))
+  )
+
+  val combinedChecks:CombinedParValidations = Seq()
+
+// TODO: sorted = boolean !
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+
+    val showHelp = Try(config.getString("help")).toOption.isDefined
+    val testsSingle = runSingleParValidations(simpleChecks, config)
+    val testsCombined = runCombinedParValidations(combinedChecks, config)
+    val allTests = aggregateValidations(testsSingle ++ testsCombined)
+
+    (showHelp, allTests._1) match {
+      case (true, _) => SparkJobInvalid(helpMsg)
+      case (false, true) => SparkJobValid
+      case (false, false) => SparkJobInvalid(allTests._2)
+    }
+
+  }
+
+  override def runJob(sc: SparkContext, config: Config): Any = {
+
+    // API Version
+    val version = Try(config.getString("version")).getOrElse("v1")
+    // Compound query string
+    val signatureString:String = Try(config.getString("query")).getOrElse("")
+    val signatureQuery = signatureString.split(" ")
+    // Sort the similarities?
+//    val sortResult:Boolean = Try(config.getString("sorted").toBoolean).getOrElse(false)
+
+    // Bins in x and y
+    val binsX = Try(config.getString("binsX").toInt).getOrElse(20)
+    val binsY = Try(config.getString("binsY").toInt).getOrElse(20)
+
+    // Load cached data
+    val db = retrieveDb(sc, this)
+    val genes = retrieveGenes(sc, this).value
+
+    // Arguments for endpoint functions
+    val input = (db, genes)
+    val parameters = (signatureQuery, binsX, binsY)
+
+    Map(
+        "info"   -> info(input, parameters),
+        "header" -> header(input, parameters),
+        "data"   -> result(input, parameters)
+      )
+
+  }
+
+}
---
+++ src/main/scala/com/dataintuitive/luciusapi/functions/BinnedZhangFunctions.scala
@@ -0,0 +1,68 @@
+package com.dataintuitive.luciusapi.functions
+
+import com.dataintuitive.luciusapi.binning.BinningFunctions._
+
+import com.dataintuitive.luciuscore.GeneModel.Genes
+import com.dataintuitive.luciuscore.Model.DbRow
+import com.dataintuitive.luciuscore.SignatureModel.SymbolSignature
+import com.dataintuitive.luciuscore.TransformationFunctions._
+import com.dataintuitive.luciuscore.ZhangScoreFunctions._
+import org.apache.spark.rdd.RDD
+
+import scala.collection.immutable.Map
+
+object BinnedZhangFunctions extends Functions {
+
+  type Input = (RDD[DbRow], Genes)
+  type Parameters = (Array[String], Int, Int)
+  type Output = Seq[Map[String, Any]]
+
+  val helpMsg =
+    s"""
+     """.stripMargin
+
+  def info(data:Input, par:Parameters) = s""
+
+  def header(data:Input, par:Parameters) = s""
+
+  def result(data:Input, par:Parameters) = {
+
+    val (db, genes) = data
+    val (rawSignature, binsX, binsY) = par
+
+    // TODO: Make sure we continue with all symbols, or just make the job invalid when it isn't!
+    val signature = SymbolSignature(rawSignature)
+
+    val symbolDict = genes.symbol2ProbesetidDict
+    val indexDict = genes.index2ProbesetidDict
+    val iSignature = signature
+      .translate2Probesetid(symbolDict)
+      .translate2Index(indexDict)
+
+    val vLength = db.first.sampleAnnotations.t.get.length
+    val query = signature2OrderedRankVector(iSignature, vLength)
+
+    // Calculate Zhang score for all entries that contain a rank vector
+    // This should be used in a flatMap
+    def updateZhang(x:DbRow, query:Array[Double]):Option[(Double, DbRow)] = {
+      x.sampleAnnotations.r match {
+        case Some(r) => Some((connectionScore(r, query), x))
+        case _ => None
+      }
+    }
+
+    val zhangAdded = db.flatMap{updateZhang(_, query)}
+    val zhangStripped = zhangAdded.map(_._1)
+
+    if (binsY > 0)
+      bin2D(zhangStripped, binsX, binsY)
+    else
+      bin1D(zhangStripped, binsX)
+
+//    result.map{case (i, z, j, pwid) => Map("indexNew" -> i, "indexOld" -> j, "zhang" -> z, "pwid" -> pwid)}
+
+  }
+
+  def binnedZhang = result _
+
+}
