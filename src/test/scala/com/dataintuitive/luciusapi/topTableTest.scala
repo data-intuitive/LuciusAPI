14c2ee94c7fa4da595759c74d0e8aaaba8f9d8ff
Hendrik Cannoodt <hendrik.cannoodt@gmail.com>
2021-10-27 11:41:09 +0200

    Add some basic tests for TopTable
    While these tests are quite straight forward, we already fail as the API should not allow head and tail

--- src/test/scala/com/dataintuitive/luciusapi/topTableTest.scala
+++ src/test/scala/com/dataintuitive/luciusapi/topTableTest.scala
@@ -44,10 +44,57 @@ class topTableTest extends FunSpec with Matchers with InitBefore {
 
       val thisConfig = ConfigFactory.parseString(configBlob).withFallback(baseConfig)
 
-      //validate(sc, thisConfig) should be (SparkJobValid)
       val jobData = topTable.validate(sparkSession, runtime, thisConfig)
 
       jobData.isGood shouldBe true
+      jobData.get.version shouldBe "v2"
+      jobData.get.specificData.head shouldBe 14
+      jobData.get.specificData.tail shouldBe 0
+      jobData.get.specificData.signatureQuery shouldBe List("HSPA1A", "DNAJB1", "BAG3", "P4HA2", "HSPA8", "TMEM97", "SPR", "DDIT4", "HMOX1", "-TSEN2")
+      jobData.get.specificData.featuresQuery shouldBe List("zhang", "jnjs", "id", "smiles")
+      jobData.get.specificData.filters shouldBe List()
+    }
+
+    it("Should not validate if the head parameter is set to 'yes'") {
+
+      // v2 interface
+      val configBlob =
+        """
+          |{
+          |  version = v2,
+          |  features = zhang jnjs id smiles,
+          |  head = yes,
+          |  query= HSPA1A DNAJB1 BAG3 P4HA2 HSPA8 TMEM97 SPR DDIT4 HMOX1 -TSEN2
+          |}
+        """.stripMargin
+
+      val thisConfig = ConfigFactory.parseString(configBlob).withFallback(baseConfig)
+
+      val jobData = topTable.validate(sparkSession, runtime, thisConfig)
+
+      jobData.isGood shouldBe false
+
+    }
+
+    it("Should not allow both head and tail") {
+
+      // v2 interface
+      val configBlob =
+        """
+          |{
+          |  version = v2,
+          |  features = zhang jnjs id smiles,
+          |  head = 14,
+          |  tail = 5
+          |  query= HSPA1A DNAJB1 BAG3 P4HA2 HSPA8 TMEM97 SPR DDIT4 HMOX1 -TSEN2
+          |}
+        """.stripMargin
+
+      val thisConfig = ConfigFactory.parseString(configBlob).withFallback(baseConfig)
+
+      val jobData = topTable.validate(sparkSession, runtime, thisConfig)
+
+      jobData.isGood shouldBe false
 
     }
 
